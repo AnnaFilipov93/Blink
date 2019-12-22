bcf683db6add15d3bbd794cbf45885ca14505f26
matan1905 <stylematan@gmail.com>
2019-12-22 17:54:09 +0200

    cone collider > raycast

---
+++ Assets/ConeCollider/Script/ConeColliderShine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConeColliderShine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        ShineReset(other.gameObject);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        ShineReset(other.gameObject);
+    }
+
+    void ShineReset(GameObject hit) {
+        if (!hit.tag.StartsWith("Enemy")) return;
+        var ai = hit.transform.GetComponent<BasicAI>();
+        if (ai)
+        {
+            ai.timeSinceShine = 0;
+        }
+    }
+}
